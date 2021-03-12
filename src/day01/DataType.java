2bc2cf0023aea7fd99d74b5741063994c787c086
felizju <felizju@gmail.com>
2021-03-12 16:00:08 +0900

    [21.03.12] #Casting 학습 완료

--- src/day01/DataType.java
+++ src/day01/DataType.java
@@ -2,7 +2,8 @@ package day01;
 
 public class DataType {
     public static void main(String[] args) {
-
+        
+        // 정수형 데이터타입
         byte a = 127;
         short b = 32767;
         int c = 2147483647;
@@ -15,12 +16,38 @@ public class DataType {
             n += 0.1;
         }
         System.out.println("0.1을 100번 더한 결과" + n);
-
+    
+        // 실수형 데이터타입
         float f1 = 3.74564654687521F; // 4byte - 접미사 'F' 붙임.
         double d1 = 3.7465468765415; // 8byte
 
         System.out.println(f1);
         System.out.println(d1);
+
+        // 논리형 데이터타입
+        boolean b1 = true;
+        boolean b2 = false;
+//        boolean b3 = 0; // 0,1 값으로 논리형 분류 불가
+//        boolean b4 = null;
+//        boolean b5 = "true";
+//        boolean b6 = True; // 대문자 True 불가
+
+        // 문자형 데이터타입
+        // char 타입은 문자 1개를 저장할 수 있는 타입, 홑따옴표로 저장
+        char c1 = 'A';
+        // String 타입은 문자열을 저장할 수 있는 타입, 곁따옴표로 저장
+        // char 타입의 배열 형태 char value[];
+        String s1 = "ABC";
+        String s2 = "A";
+
+        System.out.println("100"+"200");
+        System.out.println(100+"200");  // 자동 형변환 : 문자열
+//        System.out.println("200"-100); // 숫자와 문자열의 덧셈만 문자열 반환처리
+
+
+
+        
+
 
     }
 }
---
+++ src/day02/Casting2.java
@@ -0,0 +1,29 @@
+package day02;
+
+public class Casting2 {
+    public static void main(String[] args) {
+
+        char c = 'B';   // B : ASCII Code 66
+        int i = 2;
+        System.out.println(c + i); // 큰쪽인 int 타입에 맞춤.
+
+        char result = (char) (c + i);
+        System.out.println("result = " + result);
+
+        char result2 = (char) (50000+60000);
+        System.out.println("result2 = " + result2);
+
+        // int 보다 작은 (byte, short, char)의 연산은 결과가 자동으로 int 로 변환됨.
+        byte b1 = 100;
+        byte b2 = 110;
+//        byte b3 = b1 + b2; // 더하면 byte 값 오버플로우
+        System.out.println('A' + 'B');
+
+        int n1 = 27;
+        int n2 = 5;
+
+        double res = (double) n1 / n2;
+        System.out.println("res = " + res);
+
+    }
+}
