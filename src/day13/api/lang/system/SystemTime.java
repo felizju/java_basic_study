c28adb39aafd62ce1b375bbcc54f2707a9431b69
felizju <felizju@gmail.com>
2021-03-31 16:06:07 +0900

    [21.03.31] interface & api 학습

--- src/day13/api/lang/system/SystemTime.java
+++ src/day13/api/lang/system/SystemTime.java
@@ -5,19 +5,19 @@ public class SystemTime {
     public static void main(String[] args) {
 
         long now = System.currentTimeMillis();
-        System.out.println("now = "+now);
+        System.out.println("now = " + now);
 
         // 알고리즘 시간 복잡도 계산
         long start = System.currentTimeMillis(); // 시작시간
 
         int total = 0;
-        for (int i = 0; i < 1000000000 ; i++) {
+        for (int i = 0; i < 1000000000; i++) {
             total += i;
         }
 
         long end = System.currentTimeMillis(); // 종료시간
 
-        System.out.println("실행시간 : "+(end-start)+"ms");
+        System.out.println("실행시간 : " + (end - start) + "ms");
 
     }
 }
